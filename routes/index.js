8ef37e04007676f2e1afc0487aaec0c611b05785
Bello Ajibola <bellohargbola13@gmail.com>
2018-01-16 06:51:27 +0100

    clean up

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,16 @@ const Admin = require('../models/adminModel');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MedFind BackEnd' });
-  // console.log(`Lets get started`);
-  var admin = new Admin({
-    fullname: 'Bello Ajibola Fuad',
-    email: 'bellohargbola13@gmail.com',
-    password: '$2a$10$HQ5w7mIQBzjFybaZ0D0/GeGFQi6gdoEa2FW.g384nLQ6QNBDq4F.G',
-    privilege: 'global'
-  });
-  admin.save().then(function (data){
-    console.log(data);
-  });
+  console.log(`Lets get started`);
+  // var admin = new Admin({
+  //   fullname: 'Bello Ajibola Fuad',
+  //   email: 'bellohargbola13@gmail.com',
+  //   password: '$2a$10$HQ5w7mIQBzjFybaZ0D0/GeGFQi6gdoEa2FW.g384nLQ6QNBDq4F.G',
+  //   privilege: 'global'
+  // });
+  // admin.save().then(function (data){
+  //   console.log(data);
+  // });
 });
 
 router.get('/api', (req, res, next)=>{
